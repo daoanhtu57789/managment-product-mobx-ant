cc706b28f6e56b3a43a51cb20683b0dc863e9f5e
tudaoanh <tuda62@wru.vn>
2020-05-24 09:19:39 +0700

    Updates

---
+++ precache-manifest.4f7e2a7a5f088efe536dedf91942d606.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5201e6fb11ea539a3f26b403e59ba260",
+    "url": "/managment-product-mobx-ant/index.html"
+  },
+  {
+    "revision": "cc9cc7b52fd353b9d340",
+    "url": "/managment-product-mobx-ant/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "53db2c45d2dc8b77828c",
+    "url": "/managment-product-mobx-ant/static/css/main.64b499bd.chunk.css"
+  },
+  {
+    "revision": "cc9cc7b52fd353b9d340",
+    "url": "/managment-product-mobx-ant/static/js/2.e63a6d85.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/managment-product-mobx-ant/static/js/2.e63a6d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53db2c45d2dc8b77828c",
+    "url": "/managment-product-mobx-ant/static/js/main.7e11f84a.chunk.js"
+  },
+  {
+    "revision": "e78cb98f9232aae992ce",
+    "url": "/managment-product-mobx-ant/static/js/runtime-main.400cfd4e.js"
+  }
+]);
